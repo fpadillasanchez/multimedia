155d9b0455b220e672015bc01e7b4a697879a93b
fpadillasanchez <fpadilsa@gmail.com>
2016-10-26 21:12:20 +0200

    Huffman Table
    Sin acabar. Modificaciones en la clase HuffmanTable.

--- Huffman/src/huffman/HuffmanTable.java
+++ Huffman/src/huffman/HuffmanTable.java
@@ -5,28 +5,34 @@
  */
 package huffman;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Sergi Diaz
  */
-
 // Sorted linked list of entries
 public class HuffmanTable {
+
+    private HashMap<String, Float> list = new HashMap<>();
+    private Entry first;
+    private int size;
+
     // Stores word and probability
     class Entry {
-        public String word; // might be a symbol or a chain of symbols
+
+        public String word; // might be a symbol, number or a chain of symbols
         public float prob;  // probability
-        public Entry next;  // next entry
-        
-        public Entry (String word, float prob) {
+        public Entry next;  // next entry pointer
+
+        public Entry(String word, float prob) {
             this.word = word;
             this.prob = prob;
             next = null;
         }
-        
+
         public boolean isBefore(Entry other) {
             if (other == null) {
                 return false;
@@ -34,29 +40,59 @@ public class HuffmanTable {
             return this.prob < other.prob;
         }
     }
-    
-    private Entry first;
-    private int size;
-    
-    public HuffmanTable () {
+
+    public HuffmanTable() {
         first = null;
         size = 0;
     }
-    
-    public static HuffmanTable setList(HashMap<String, Float> hash) {
-        HuffmanTable list = new HuffmanTable();
-        for (HashMap.Entry<String, Float> e : hash.entrySet()) {
-            list.addEntry( e.getKey(), e.getValue() );
-            list.size ++;
-        }
-        return list;
+
+    public void setList(String word, float prob) {
+        list.put(word, prob);
     }
-    
+
+    public void asdf() {
+        for (Map.Entry<String, Float> e : list.entrySet()) {
+            addEntry(e.getKey(), e.getValue());
+            size++;
+        }
+    }
+
+    public void buildTree(HuffmanTree tree) {
+        //estructura de datos temporal para guardar los nodos
+        HashMap<String, Float> dictionary = new HashMap<>();
+        Entry probe = first;
+        dictionary = enterNodes(dictionary); //a,b,c,d and e (first iteration)
+        while (probe.next != null) {
+            // Nodos unidos tienen la misma probabilidad, los unimos
+            if (probe.prob == probe.next.prob) {
+                double probabilitat = probe.prob;
+                String word = probe.word;
+                probe.next.prob += probabilitat;
+                probe.next.word += word;
+                probe.next.word = sortString(probe.next.word);
+                dictionary.put(probe.next.word, probe.next.prob);
+                removeFirst();
+                //una vez avanzado el proceso y nos queden dos nodos, 0.6 y 0.4 
+                //por ejemplo, los unimos en el nodo root del árbol
+            } else if (probe.prob != probe.next.prob && size == 2) {
+                double probabilitat = probe.prob;
+                String word = probe.word;
+                probe.next.prob += probabilitat;
+                probe.next.word += word;
+                probe.next.word = sortString(probe.next.word);
+                dictionary.put(probe.next.word, probe.next.prob);
+                removeFirst();
+            } else {
+                probe = probe.next;
+            }
+        }
+    }
+
     // Sorted insertion
-    public void addEntry (String word, float prob) {
+    public void addEntry(String word, float prob) {
         Entry entry = new Entry(word, prob);
         size++;
-        
+
         if (first == null) {
             first = entry;
             return;
@@ -66,7 +102,7 @@ public class HuffmanTable {
             first = entry;
             return;
         }
-        
+
         // Sorted from low to high probability
         Entry probe = first;
         while (probe.next != null) {
@@ -78,37 +114,63 @@ public class HuffmanTable {
             probe = probe.next;
         }
         probe.next = entry;
-    } 
-    
-    public void removeFirst () {
+    }
+
+    public void removeFirst() {
         if (first != null) {
             first = first.next;
             size--;
         }
     }
-    
+
+    public Entry removeLast() {
+        Entry probe = first;
+        Entry previousProbe = null;
+        while (probe.next != null) {
+            if (probe.next == null) {
+                previousProbe.next = null; //one entry removed from last position
+                size--;
+                return probe;
+            }
+            previousProbe = probe;
+            probe = probe.next;
+
+        }
+
+        return probe;
+    }
+
+    private HashMap<String, Float> enterNodes(HashMap<String, Float> dictionary) {
+        Entry probe = first;
+        while (probe.next != null) {
+            dictionary.put(probe.word, probe.prob);
+            probe = probe.next;
+        }
+        return dictionary;
+    }
+
     public String getFirstWord() {
         if (first == null) {
             return null;
         }
         return first.word;
     }
-    
+
     public float getLowestProbability() {
         if (first == null) {
             return 0f;
         }
         return first.prob;
     }
-    
+
     public boolean isEmpty() {
         return first == null;
     }
-    
-    public int size () {
+
+    public int size() {
         return size;
     }
-    
+
     public HashMap<String, Float> getTable() {
         if (first == null) {
             return null;
@@ -121,11 +183,11 @@ public class HuffmanTable {
         }
         return table;
     }
-    
+
     @Override
     public String toString() {
         String str = "";
-        
+
         Entry entry = first;
         while (entry != null) {
             str += (entry.word + ": " + entry.prob + "\n");
@@ -133,5 +195,15 @@ public class HuffmanTable {
         }
         return str;
     }
-    
+
+    /*
+        Sorted (char) string array
+     */
+    private String sortString(String word) {
+
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return String.valueOf(chars);
+    }
+
 }
--- Huffman/src/huffman/Main.java
+++ Huffman/src/huffman/Main.java
@@ -1,7 +1,5 @@
 package huffman;
 
-import java.util.HashMap;
-
 
 public class Main {
 
@@ -10,14 +8,30 @@ public class Main {
      */
     public static void main(String[] args) {
 
-        HashMap<String, Float> lang = new HashMap<>(); // llenguatge  a codificar
-        lang.put("A", 0.4f);    // A = 0.4
+        char[] abecedario = createArray();
+        float prob[][] = {
+            {0.3f, 0.3f, 0.2f, 0.1f, 0.1f},
+            {0.6f, 0.2f, 0.1f, 0.05f, 0.05f},
+            {0.5f, 0.2f, 0.1f, 0.05f, 0.05f},
+            {0.4f, 0.3f, 0.2f, 0.05f, 0.05f}
+        };
+        //HashMap<String, Float> lang = setProbabilites(abecedario,prob); // llenguatge  a codificar
+        /*lang.put("A", 0.4f);    // A = 0.4
         lang.put("B", 0.4f);    // B = 0.4
         lang.put("C", 0.1f);    // C = 0.1
         lang.put("D", 0.05f);   // D = 0.05
         lang.put("E", 0.05f);   // E = 0.05
-        
+         */
         HuffmanTree tree = new HuffmanTree();
+        /*
+         This is gonna be random
+        */
+        HuffmanTable table = new HuffmanTable();
+        for (int i = 0; i < 5; i++) {
+            table.setList(String.valueOf(abecedario[i]),prob[0][i]);
+        }
+        table.asdf();
+        table.buildTree(tree);
         /*
         L'arbre implementat està pensat per rebre com a input cada element de les
         taules de probabilitats que es generen fent servir el algorisme de Huffman.
@@ -26,7 +40,7 @@ public class Main {
         
         Aquesta part encara no tinc clar com fer-la, he estat provant amb llistes
         enllaçades (classe HuffmanTable).
-        */
+         */
         tree.addNode("ABCDE", 1f);
         // 3a iteració
         tree.addNode("BCDE", 0.6f);
@@ -37,21 +51,36 @@ public class Main {
         tree.addNode("CDE", 0.2f);
         // 1a iteració
         tree.addNode("A", 0.4f);
-        tree.addNode("B", 0.4f); 
+        tree.addNode("B", 0.4f);
         tree.addNode("C", 0.1f);
         tree.addNode("DE", 0.1f);
         // inici
         tree.addNode("A", 0.4f);
-        tree.addNode("B", 0.4f); 
+        tree.addNode("B", 0.4f);
         tree.addNode("C", 0.1f);
         tree.addNode("D", 0.05f);
         tree.addNode("E", 0.05f);
-        
-        // Mostra per pantalla el codi de cada lletra del llenguatge:
+
+        /* Mostra per pantalla el codi de cada lletra del llenguatge:
         for (String symbol : lang.keySet()) {
             System.out.println(symbol + " = " + tree.getCode(symbol, 0));
         }
-        
+        */
+
     }
-    
+
+    /**
+     * Create an array filled with the letter from the alphabet
+     *
+     * @return
+     */
+    private static char[] createArray() {
+        char[] s = new char[26];
+        for (int letra = 0; letra < 26; letra++) {
+            s[letra] = (char) ('A' + letra);
+
+        }
+        return s;
+    }
+
 }
