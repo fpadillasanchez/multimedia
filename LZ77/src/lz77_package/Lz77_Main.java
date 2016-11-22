bbbb7ca114619f4238d6b5cbbb28565983e49545
fpadillasanchez <fpadilsa@gmail.com>
2016-11-22 13:40:48 +0100

    LZ77 con texto

--- LZ77/src/lz77_package/Lz77_Main.java
+++ LZ77/src/lz77_package/Lz77_Main.java
@@ -1,11 +1,9 @@
 package lz77_package;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import static lz77_package.txtReader.cargarTxt;
+import static lz77_package.txtReader.ASCIIbin2string;
 
 /**
  * Fernando Padilla, Sergi Diaz
@@ -17,21 +15,17 @@ public class Lz77_Main {
     private static int Mdes = 8;        // size of the sliding window
     private static int entrySize = 25;  // size of the input string
     private static String fileEntry = "";
+    private static StringBuffer bits;
 
     @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
+
         System.out.println("Starting...");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter file input: ");
         fileEntry = sc.nextLine();
-        System.out.print("Enter the binary sequence length : ");
-        try {
-            entrySize = sc.nextInt();
-        } catch (InputMismatchException e) {;
-            System.out.println("Length set as default " + entrySize);
-            sc.next();
-        }
-
+        bits = cargarTxt("src/resources/"+fileEntry);
+        
         System.out.print("Enter the INPUT WINDOW size: ");
         try {
             Ment = sc.nextInt();;
@@ -52,7 +46,7 @@ public class Lz77_Main {
             System.exit(0);
         } else {
             LZ77 lz77 = new LZ77(Mdes, Ment);
-            String compressed_bits = lz77.compress(bits);
+            String compressed_bits = lz77.compress(bits.toString());
             if (compressed_bits == null) {
                 System.out.println("Error Compressing, Found no matches in the window.");
 
@@ -69,6 +63,7 @@ public class Lz77_Main {
                 System.out.println("Decompressed sequence: " + bits);
                 if (bits.equals(decompressed_bits)) {
                     System.out.println("Compression completed");
+                    System.out.println("Decompressed sequence: "+ ASCIIbin2string(new StringBuffer(compressed_bits)));
                 } else {
                     System.out.println("Compression error");
                 }
@@ -89,7 +84,7 @@ public class Lz77_Main {
             System.out.println("Ment>Mdes");
             return false;
         }
-        if (Mdes + Ment > entrySize) {
+        if (Mdes + Ment > bits.length()) {
             System.out.println("Mdes+Ment>entrySize");
             return false;
         }
@@ -116,31 +111,4 @@ public class Lz77_Main {
         return (x != 0) && ((x & (x - 1)) == 0);
     }
 
-    private void readFiles(String path) {
-        BufferedReader br = null;
-        File file = new File("src/recources/" + path);
-        file.getPath();
-
-        try {
-
-            String sCurrentLine;
-
-            br = new BufferedReader(new FileReader("C:\\testing.txt"));
-
-            while ((sCurrentLine = br.readLine()) != null) {
-                System.out.println(sCurrentLine);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-    }
 }
