562f635ad6dddf95e1f76385031fc40e0c79c1bd
sdiaz7 <k0n0k.wiki@gmail.com>
2017-01-11 19:16:02 +0100

    Some changes

--- CodecApplication/src/codec/Decoder.java
+++ CodecApplication/src/codec/Decoder.java
@@ -8,6 +8,7 @@ package codec;
 import control.CodecConfig;
 import io.FileIO;
 import io.FrameData;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -18,7 +19,7 @@ import java.io.IOException;
  * @author SDP
  */
 public class Decoder {
-    
+
     public static void decode(String input, String output) throws IOException, FileNotFoundException, ClassNotFoundException {
         // Abort if input is not a video generated using this codec
         /*
@@ -26,40 +27,40 @@ public class Decoder {
             // TODO: throw custom codec exception
             return;
         }*/
-    
+
         File temp_dir;  // directory used for storing temporary files
-        
+
         temp_dir = new File(output + File.separator + CodecConfig.decoder_sub_directory);
         temp_dir.mkdir();         // create temporary directory
-        
+
         FileIO.decompress(input, temp_dir.getAbsolutePath()); // extract frame data into the tenp directory
-        
+
         for (File file : temp_dir.listFiles()) {
             // Read frame data from the extracted temporary file
-            FrameData frame = FrameData.load(file.getAbsolutePath()); 
-            
+            FrameData frame = FrameData.load(file.getAbsolutePath());
+
             // TODO: extend decoding 
             String image_path = output + File.separator + Integer.toString(frame.getId());
             FileIO.writeImage(retrieveImage(frame), image_path);
-            
+
             file.delete();  // delete temporary file
         }
-        
+
         temp_dir.delete();  // delete temporary directory
     }
-    
+
     // TODO: Retrieve images using movements and tiling matrixes
     public static BufferedImage retrieveImage(FrameData data) {
         return data.getImage();
     }
-    
+
     public static BufferedImage retrieveImage(FrameData reference, FrameData other) {
-        
+
         // TODO: Move this segment outside to avoid tiling reference multiple times
         // Process frame tilemaps
         int refer_tilemap[][];      // reference tilemap
         int other_tilemap[][];      // other frame tilemap
-        
+
         refer_tilemap = MotionCompensator.tesselate(reference.getImage());
         other_tilemap = MotionCompensator.tesselate(other.getImage());
         reference.setTileMap(refer_tilemap);
@@ -69,24 +70,35 @@ public class Decoder {
         // Tile size in pixels
         int w = other.getImage().getWidth() / CodecConfig.n_tiles_x;
         int h = other.getImage().getHeight() / CodecConfig.n_tiles_y;
-        
-        for (int i=0; i < CodecConfig.n_tiles_x; i++) {
-            for (int j=0; j < CodecConfig.n_tiles_y; j ++) {
+
+        for (int i = 0; i < CodecConfig.n_tiles_x; i++) {
+            for (int j = 0; j < CodecConfig.n_tiles_y; j++) {
                 // Obtain movements associated to the tile
                 int mov[] = other.getMovements()[i][j];
                 // Displace tile in reference and move into the other frame
-                if(mov[0] == 0) {
-                    try {
-                        int[][][] fragment = reference.getImageFragment((i + mov[1]) * w, (j + mov[2]) * h);
-                        other.setImageFragment(fragment, i * w, j * h);
-                    } catch (Exception ex){
-                        // Out of range
-                    }      
+                
+                if (mov[0] == 0 || true) {
+                    
+                    for (int x = (i * w); x < ((i + 1) * w); x++) {
+                        for (int y = (j * h); y < ((j + 1) * h); y++) {
+                            
+                            
+                            try {
+                                
+                                int color = reference.getImage().getRGB(x, y);
+                                other.getImage().setRGB(x + mov[i], y + mov[j], color);
+
+                                //System.out.println(x +", " + y);
+                            } catch (Exception ex) {
+                                // Out of range
+                            }
+                        }
+                    }
                 }
             }
         }
-        
+
         return other.getImage();
-    }  
-    
+    }
+
 }
